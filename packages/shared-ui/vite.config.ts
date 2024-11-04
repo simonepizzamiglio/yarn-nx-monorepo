569212a75163ad2fe22619339797f445ca5fc957
Simone Pizzamiglio <simonepizzamiglio@me.com>
2024-11-04 16:00:51 +0100

    fixes

--- packages/shared-ui/vite.config.ts
+++ packages/shared-ui/vite.config.ts
@@ -1,12 +1,18 @@
-import path from "path"
 import react from "@vitejs/plugin-react"
 import {defineConfig} from "vite"
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+  build: {
+    lib: {
+      // Use the name field from package.json as the entry point
+      entry: "src/index.ts",
+      formats: ["es", "cjs"],
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      // Externalize React and ReactDOM
+      external: ["react", "react-dom"],
     },
   },
+  plugins: [react()],
 })
